a12fa36bc7cee584c756a370d7c9f57fd2243a61
Marco van Zwetselaar <io@zwets.it>
2022-09-18 02:47:42 +0000

    Final working setup fixes

--- src/setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup, find_packages
-setup(name='resfinder', packages=find_packages(exclude=['scripts','tests']), entry_points={"console_scripts": ['resfinder = resfinder.run_resfinder:main']}, zip_safe=False,
-      version='4.2.1.2'
-      )
